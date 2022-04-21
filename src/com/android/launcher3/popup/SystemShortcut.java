e21df3de4b584f154d790023b4ed63ad7e92cf62
jhonboy121 <alfredmathew05@gmail.com>
2022-04-21 20:42:27 +0530

    launcher3: SystemShortcut: abort uninstall action if package name is null
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -234,10 +234,12 @@ public abstract class SystemShortcut<T extends Context & ActivityContext> extend
         }
     }
 
-    public static final Factory<BaseDraggingActivity> UNINSTALL = (activity, itemInfo) ->
-            PackageManagerHelper.isSystemApp(activity,
-                 itemInfo.getTargetComponent().getPackageName())
-                    ? null : new UnInstall(activity, itemInfo);
+    public static final Factory<BaseDraggingActivity> UNINSTALL = (activity, itemInfo) -> {
+        final String packageName = itemInfo.getTargetPackage();
+        if (packageName == null) return null;
+        return PackageManagerHelper.isSystemApp(activity, packageName)
+            ? null : new UnInstall(activity, itemInfo);
+    };
 
     public static class UnInstall extends SystemShortcut<BaseDraggingActivity> {
 
